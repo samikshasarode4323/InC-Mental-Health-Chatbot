e4e8b924eb713d2856b89d89a0f0a483b756a374
varadpundlik <varadpundlik@gmail.com>
2023-04-13 21:49:13 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// import AddMedPage from "./pages/addMedPage";
+// import MedDashboard from "./pages/MedDashboard";
+// import Status from "./pages/statusPage";
+function App() {
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<MedDashboard />} />
+    //     <Route path='/addmed' element={<AddMedPage />} />
+    //     <Route path='/status' element={<Status />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
