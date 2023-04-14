5e1d0d6b0cd4d9104173defc9d6cbb0d4e9108ed
Manav2031 <92452757+Manav2031@users.noreply.github.com>
2023-04-14 09:52:13 +0000

    updated navbar

---
+++ client/src/pages/NewChat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewChat() {
+    return (
+        <div className="newchat">
+            <h1>New Chat</h1>
+        </div>
+    )
+}
+
+export default NewChat;
