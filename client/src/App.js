16116be13783b930ed86f0296b3ab7cefbfa0126
Manav2031 <92452757+Manav2031@users.noreply.github.com>
2023-04-15 14:18:53 +0000

    updated chat history

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import NewChat from "./pages/NewChat";
 import Login from "./pages/Login";
+import ChatHistory from "./pages/ChatHistory";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/newchat' element={<NewChat />} /> 
         <Route path='/login' element={<Login />} />
+        <Route path='/chathistory' element={<ChatHistory />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -60,7 +60,7 @@ function Login() {
       return (
         <div className="app">
           <div className="login-form">
-            <div className="title">Sign In</div>
+            <div className="title">Login</div>
             {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
           </div>
         </div>
